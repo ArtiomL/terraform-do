16df5066e40151c623b8128763a2b0a2d9a565e7
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-06 22:37:10 +0300

    Firewall Input Variables

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,1 +1,21 @@
+# Firewall Input Variables
 
+variable "firewall_name" {
+  description = "Firewall name"
+  default     = "Dam"
+}
+
+variable "droplet_ids" {
+  description = "Droplet IDs assigned to the Firewall"
+  default     = []
+}
+
+variable "mgmt_asrc" {
+  description = "Source IPv4 CIDR block(s) allowed to access management"
+  default     = []
+}
+
+variable "tags_shared" {
+  description = "Tags assigned to all resources"
+  default     = ["env:prod", "owner:artioml", "project:nautilus"]
+}
