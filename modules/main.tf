10428f1b546511ce78a0074b2513a25f332e1cc1
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-06 04:00:17 +0300

    DigitalOcean Resources

--- modules/main.tf
+++ modules/main.tf
@@ -1,21 +1,16 @@
 # DigitalOcean Resources
 
-locals {
-  tags = ["Terraform", "${var.tag_environment}"]
-}
-
 provider "digitalocean" {}
 
-resource "digitalocean_droplet" "main" {
-  ssh_keys = "${var.ssh_keys}"
-  image    = "${var.droplet_image}"
-  region   = "${var.do_region}"
-  size     = "${var.droplet_size}"
-  name     = "drop${var.tag_name}${var.tag_environment}"
-  tags     = "${concat(local.tags, var.tags_shared)}"
-}
-
-resource "digitalocean_floating_ip" "main" {
-  droplet_id = "${digitalocean_droplet.main.id}"
-  region     = "${digitalocean_droplet.main.region}"
+# Droplet
+module "droplet" {
+  source        = "./droplet"
+  do_region     = "${var.do_region}"
+  key_path      = "${var.key_path}"
+  key_name      = "${var.key_name}"
+  droplet_names = "${var.droplet_names}"
+  droplet_image = "${var.droplet_image}"
+  droplet_size  = "${var.droplet_size}"
+  user_data     = "${var.user_data}"
+  tags_shared   = "${var.tags_shared}"
 }
