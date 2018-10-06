bee783cd977d9390ef89dc810f01b50bef8e5f29
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-06 22:24:30 +0300

    DigitalOcean Resources

--- modules/main.tf
+++ modules/main.tf
@@ -4,14 +4,20 @@ provider "digitalocean" {}
 
 # Droplet
 module "droplet" {
-  source        = "./droplet"
-  do_region     = "${var.do_region}"
-  key_path      = "${var.key_path}"
-  key_name      = "${var.key_name}"
-  droplet_names = "${var.droplet_names}"
-  droplet_image = "${var.droplet_image}"
-  droplet_size  = "${var.droplet_size}"
-  floating_ip   = "${var.floating_ip}"
-  user_data     = "${var.user_data}"
-  tags_shared   = "${var.tags_shared}"
+  source          = "./droplet"
+  do_region       = "${var.do_region}"
+  key_path        = "${var.key_path}"
+  key_name        = "${var.key_name}"
+  droplet_names   = "${var.droplet_names}"
+  droplet_image   = "${var.droplet_image}"
+  droplet_size    = "${var.droplet_size}"
+  droplet_backups = "${var.droplet_backups}"
+  floating_ip     = "${var.floating_ip}"
+  user_data       = "${var.user_data}"
+  tags_shared     = "${var.tags_shared}"
+}
+
+# Public IP
+module "ifconfig" {
+  source = "github.com/ArtiomL/f5-terraform/modules/common/ifconfig"
 }
