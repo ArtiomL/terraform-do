19642b757b0972bda6c7697ba45c31571679c908
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-10 00:35:46 +0300

    Droplet Resources

--- modules/droplet/main.tf
+++ modules/droplet/main.tf
@@ -8,7 +8,7 @@ resource "digitalocean_droplet" "main" {
   count     = "${length(var.droplet_names)}"
   ssh_keys  = ["${digitalocean_ssh_key.main.fingerprint}"]
   image     = "${var.droplet_image}"
-  region    = "${var.do_region}"
+  region    = "${element(var.do_region, count.index)}"
   size      = "${var.droplet_size}"
   name      = "${element(var.droplet_names, count.index)}"
   backups   = "${var.droplet_backups}"
@@ -19,7 +19,7 @@ resource "digitalocean_droplet" "main" {
 resource "digitalocean_floating_ip" "main" {
   count      = "${var.floating_ip ? length(var.droplet_names) : 0}"
   droplet_id = "${element(digitalocean_droplet.main.*.id, count.index)}"
-  region     = "${var.do_region}"
+  region     = "${element(digitalocean_droplet.main.*.region, count.index)}"
 }
 
 resource "digitalocean_ssh_key" "main" {
