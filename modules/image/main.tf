5cc55336b8228f10077bcdc1c7dc856783948458
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2018-10-20 03:20:36 +0300

    Image Data Source

--- modules/image/main.tf
+++ modules/image/main.tf
@@ -1,5 +1,5 @@
 # Image Data Source
 
 data "external" "image" {
-  program = ["sh", "-c", "curl -sL '${var.images_url}' -H 'Authorization: Bearer ${var.digitalocean_token}' | jq --arg image_dist ${var.image_dist} '.images[] | select(.distribution == $image_dist and (.description | startswith($image_dist)) and .public == true)' | jq -s -c 'sort_by(.description) | .[-1]' | jq '{slug: .slug}'"]
+  program = ["sh", "-c", "curl -sL '${var.images_url}' -H 'Authorization: Bearer ${var.digitalocean_token}' | jq --arg image_dist ${var.image_dist} '.images[] | select(.distribution == $image_dist and (.description | startswith($image_dist)) and .public == true)' | jq -s -c 'sort_by(.description) | .[-1]' | jq '{id: .id, slug: .slug}'"]
 }
